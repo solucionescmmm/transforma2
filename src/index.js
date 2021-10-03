a875c603aa0ea8259326b3385c4c2f81f70fbe64
Santiago Cardona Saldarriaga <cardona0496@gmail.com>
2021-10-03 14:07:25 -0500

    [core] Actualizacion de archivos

--- src/index.js
+++ src/index.js
@@ -1,35 +1,10 @@
-//Paquetes
-require("dotenv-flow").config();
+//Servidor
+const clsServer = require("./modules/Main/app/server");
 
-//Librerias
-const https = require("https");
-const fs = require("fs");
+const init = () => {
+    let server = new clsServer();
 
-//Modulos
-const app = require("./modules/Main/app");
-
-const Main = () => {
-    //console.log(app.get("typeServer"));
-
-    if (app.get("typeServer") === "production") {
-        https
-            .createServer(
-                {
-                    key: fs.readFileSync(
-                        "/var/www/servicios.demismanos.org/ssl/servicios.demismanos.org-le.key"
-                    ),
-                    cert: fs.readFileSync(
-                        "/var/www/servicios.demismanos.org/ssl/servicios.demismanos.org-le.crt"
-                    ),
-                },
-                app
-            )
-            .listen(app.get("port"), () => {
-                console.log("HTTPS server listening on port " + app.get("port") + "...");
-            });
-    } else {
-        app.listen(app.get("port"));
-    }
+    server.main();
 };
 
-Main();
+init();
---
+++ src/modules/Main/app/server.js
@@ -0,0 +1,74 @@
+//Paquetes
+require("dotenv-flow").config();
+
+//Librerias
+const https = require("https");
+const http = require("http");
+const fs = require("fs");
+const socketIo = require("socket.io");
+
+//Modulos
+const app = require("./index");
+
+class clsServer {
+    #objServer;
+
+    async main() {
+        await this.#server();
+        this.#socket();
+    }
+
+    async #server() {
+        if (app.get("typeServer") === "production") {
+            this.#objServer = https
+                .createServer(
+                    {
+                        key: fs.readFileSync(
+                            "/var/www/servicios.demismanos.org/ssl/servicios.demismanos.org-le.key"
+                        ),
+                        cert: fs.readFileSync(
+                            "/var/www/servicios.demismanos.org/ssl/servicios.demismanos.org-le.crt"
+                        ),
+                    },
+                    app
+                )
+                .listen(app.get("port"), () => {
+                    console.log(
+                        "HTTPS server listening on port " + app.get("port") + "..."
+                    );
+                });
+        } else {
+            this.#objServer = http.createServer(app).listen(app.get("port"));
+        }
+    }
+
+    #socket() {
+        let arrComentarios = [];
+
+        const io = socketIo(this.#objServer, {
+            cors: {
+                origin: "http://localhost:3000",
+                methods: ["GET", "POST", "DELETE", "PUT"],
+            },
+        });
+
+        io.on("connection", (client) => {
+            client.on("mdlComentarios:setComentario", (data) => {
+                arrComentarios.push({
+                    strTipo: data.strTipo,
+                    srtMensaje: data.srtMensaje,
+                    dtFechaCreacion: new Date(),
+                    strUsuario: data.strUsuario,
+                    strUsuarioAsignado: "",
+                    strURLImagenUsuario: data.strURLImagenUsuario,
+                });
+
+                client.emit("mdlComentarios:setComentario", arrComentarios);
+            });
+
+            client.emit("mdlComentarios:getComentarios", arrComentarios);
+        });
+    }
+}
+
+module.exports = clsServer;
