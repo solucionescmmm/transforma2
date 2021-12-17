914ce703507fbe6bec6f93b86b03953d9e8378bf
slondonog <68976097+Snayder2@users.noreply.github.com>
2021-12-17 14:56:48 -0500

    [core]Se arreglo el error del servicio de obtener usuario

--- src/common/config/confSQL_connectionSecurityTransforma.js
+++ src/common/config/confSQL_connectionSecurityTransforma.js
@@ -12,4 +12,4 @@ const conexionSecury = {
   parseJSON: true,
 };
 
-module.exports = conexionSecury;
+module.exports = {conexionSecury};
--- src/modules/Usuarios/domain/getUsuarios.service.js
+++ src/modules/Usuarios/domain/getUsuarios.service.js
@@ -2,6 +2,10 @@
 const classInterfaceDAOUsuarios = require("../Infra/conectors/interfaceDAOUsuarios");
 
 const getUsuarios = async(data) => {
+    if (!data.strApp) {
+        throw new Error("Se esperaban parametros de entrada")
+    }
+    
     let dao = new classInterfaceDAOUsuarios();
     let result = await dao.getUsuarios(data);
     return result;
