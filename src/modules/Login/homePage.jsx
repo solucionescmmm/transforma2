9f9010d4e112acf137abe05b18adf4a254ae9a75
Santiago Cardona Saldarriaga <cardona0496@gmail.com>
2024-01-15 16:49:00 -0500

    feat(ajustes) - Se ajusta el login

--- src/modules/Login/homePage.jsx
+++ src/modules/Login/homePage.jsx
@@ -253,6 +253,7 @@ const Login = () => {
                                         }}
                                     >
                                         {process.env.REACT_APP_NODE_ENV ===
+                                        "development" || process.env.REACT_APP_NODE_ENV ===
                                         "production" ? (
                                             <MicrosoftLogin
                                                 clientId={
